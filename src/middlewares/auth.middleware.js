78241e0e37353e0c330b009a70210b8cea871af9
Premansh Tripathi <98167095+premanshtripathi@users.noreply.github.com>
2026-02-04 04:29:29 +0530

    Fix typo in auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -10,7 +10,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
       req.header("Authorization")?.replace("Bearer ", "");
 
     if (!token) {
-      throw new ApiError(401, "Unautherized request!");
+      throw new ApiError(401, "Unauthorized request!");
     }
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET); // check if await is required.
