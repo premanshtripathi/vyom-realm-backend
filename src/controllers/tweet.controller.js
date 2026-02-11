90a8b03c9e036104a7c5b7016b4e3ff7c7fa510a
Premansh Tripathi <98167095+premanshtripathi@users.noreply.github.com>
2026-02-11 23:32:09 +0530

    feat(Tweet): implement createTweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,29 +1,45 @@
-import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { Tweet } from "../models/tweet.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
-})
+  //TODO: create tweet
+  const content = req.body?.content?.trim();
+  const owner = req.user?._id;
+  if (!content) {
+    throw new ApiError(400, "Content must not be empty!");
+  }
+  if (!owner) {
+    throw new ApiError(
+      400,
+      "User must be logged in to create a tweet, check the auth middleware"
+    );
+  }
+  const tweet = await Tweet.create({
+    content,
+    owner,
+  });
+  if (!tweet) {
+    throw new ApiError(500, "Something went wrong while creating the tweet!");
+  }
+  return res
+    .status(201)
+    .json(new ApiResponse(201, tweet, "Tweet created successfully."));
+});
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
-})
+  // TODO: get user tweets
+});
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
-})
+  //TODO: update tweet
+});
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
-})
+  //TODO: delete tweet
+});
 
-export {
-    createTweet,
-    getUserTweets,
-    updateTweet,
-    deleteTweet
-}
+export { createTweet, getUserTweets, updateTweet, deleteTweet };
