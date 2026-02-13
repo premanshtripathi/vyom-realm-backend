ac83826f5c7b23ac24f45f3c89df54db15afea1f
Premansh Tripathi <98167095+premanshtripathi@users.noreply.github.com>
2026-02-13 23:06:12 +0530

    add pagination and indexing in tweet model and make owner field required

--- src/models/tweet.model.js
+++ src/models/tweet.model.js
@@ -1,17 +1,23 @@
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const tweetSchema = new Schema(
   {
     content: {
       type: String,
-      required: true,
+      required: [true, "Content field must not be empty!"],
     },
     owner: {
       type: Schema.Types.ObjectId,
       ref: "User",
+      required: [true, "Owner field must not be empty!"],
     },
   },
   { timestamps: true }
 );
+
+tweetSchema.index({ owner: 1, createdAt: -1 });
+
+tweetSchema.plugin(mongooseAggregatePaginate);
 
 export const Tweet = mongoose.model("Tweet", tweetSchema);
